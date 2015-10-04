aab856ae3b79bea5e20387f8ef543889c58101db
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2015-10-04 13:51:35 -0500

    repointing to mongolab db

--- database.js
+++ database.js
@@ -1,4 +1,9 @@
+// var db = require('mongoskin')
+//   .db('mongodb://localhost:27017/savantory');
+
+// mongodb://<dbuser>:<dbpassword>@ds041160.mongolab.com:41160/savantory
+
 var db = require('mongoskin')
-  .db('mongodb://localhost:27017/savantory');
+  .db('mongodb://firerishi:firebolt@ds041160.mongolab.com:41160/savantory');
 
 module.exports = db
