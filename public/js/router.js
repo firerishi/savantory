ab1ae2956677cd50bb979fa29334a4496bf5b9aa
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2015-10-04 21:55:24 -0500

    Fixing front-end routing.

--- public/js/router.js
+++ public/js/router.js
@@ -5,13 +5,15 @@ define([
   , 'backbone'
   , 'views/LandingView'
   , 'views/HomeView'
-], function($, _, Backbone, LandingView, HomeView) {
+  , 'views/BookView'
+], function($, _, Backbone, LandingView, HomeView, BookView) {
   
   var AppRouter = Backbone.Router.extend({
     routes: {
 
-      'home': 'showHome'
-      , '*actions': 'defaultAction'
+      'home': 'showHome',
+      'book': 'showBooks',
+      '*actions': 'defaultAction'
 
     }
   });
@@ -20,15 +22,21 @@ define([
 
     var app_router = new AppRouter;
 
+    app_router.on('route:showHome', function (actions) { 
+      var homeView = new HomeView();
+      homeView.render();
+    });
+
+    app_router.on('route:showBooks', function (actions) { 
+      console.log('book route')
+      var bookView = new BookView();
+      bookView.render();
+    });   
+
     app_router.on('route:defaultAction', function (actions) { 
       var landingView = new LandingView();
       landingView.render();
     });
-
-    app_router.on('route:showHome', function (actions) { 
-      var homeView = new HomeView();
-      homeView.render();
-    });   
 
     Backbone.history.start();
   };
--- public/js/views/BookView.js
+++ public/js/views/BookView.js
@@ -3,51 +3,28 @@ define([
   'underscore',
   'backbone',
   'handlebars',
-  '../models/BookModel',
-  '../collections/BookCollection',
   'text!tpl/books.html',
   'hb_help_core'
-], function($, _, Backbone, Handlebars, BookModel, BookColl, bookHB){
+], function($, _, Backbone, Handlebars, booksHB){
 
-  var BookView = Backbone.View.extend({
+  var booksView = Backbone.View.extend({
     el: "#main-content",
-    banner: "#banner-image",
-    bannerName: "#banner-name",
+
+    events: {
+    },
 
     render: function(){
-      
-      var that = this
-        , book = new BookColl()
-        , hash = window.location.hash;
 
-      $('#main-nav li').removeClass('active');
-      $('#main-nav li a[href="'+hash+'"]').parent().addClass('active');
+      var template = Handlebars.compile(booksHB)
+        , that = this;
+        
+      $(this.el).html(template({}));
 
-      $(that.el).addClass('books');
+    },
 
-      var template = Handlebars.compile(bookHB);
-      $(that.el).html(template({book: null}));
-
-      setTimeout(function() {
-
-      book.fetch({
-        success: function() {
-          $(that.el).html(template({book: book.toJSON()}));
-
-          new AnimOnScroll( document.getElementById( 'grid' ), {
-              minDuration : 0.4,
-              maxDuration : 0.7,
-              viewportFactor : 0.2
-          } );
-        }
-      });
-
-      }, 2000);
-
-      $(this.banner).removeClass();
-
+    initialize: function() {
     }
   });
 
-  return BookView;
+  return booksView;
 });
